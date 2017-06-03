e20cf1ce5901ec2efd111efee7c05d6a00b6c5d0
MythTheWolf <myth.wolf6@gmail.com>
2017-06-02 21:21:03 -0500

    ghbhugbg7t

---
+++ JS/pageScripts/UserMail/main.js
@@ -0,0 +1,29 @@
+socket.on("message_push",function(data){
+	var JSONOBJ = JSON.parse(data);
+	var toAppend = "<div id=\"message_"
+									+ JSONOBJ.messages[i].id
+										+ "\"><li class=media><div class=media-body><div class=media><a class=pull-left href=#><img class=\"img-circle media-object\"onerror='this.src=\"/assets/image/logo-default.png\"'src=\"\"style=width:50px;height:50px></a><div class=media-body><small class=text-muted>"
+										+ JSONOBJ.messages[i].senderName									+ "</small><br>"
+										+ JSONOBJ.messages[i].content
+ 									+ "</div></div></div></li><hr></div>";
+	$("#chatbox").append(toAppend);
+	
+});
+
+$.ajax({
+	type : "POST",
+	url : "/lib/API/getConvoList.php",
+	data : $("#fieldVars").serialize(),
+	success : function(result) {
+		var OB = JSON.parse(result);
+		var arrayLength = OB.list.length;
+		for (var i = 0; i < arrayLength; i++) {
+		    var appendstr = "<li class=\"media\"> <div class=\"media-body\"> <div class=\"media\"> <a class=\"pull-left\" href=\"#\" onClick=\"setChat('" + OB.list[i].id +  "')\"> <img class=\"media-object img-circle\" style=\"height: 40px; width: 40px;\" src=\""+ OB.list[i].icon +"\"/> </a> <div class=\"media-body\"> <h5>"  + OB.list[i].user + "</h5> <small class=\"text-muted\">" + OB.list[i].content +"</small> </div> </div> </div> </li>";
+		    $("#convoList").append(appendstr);
+		}
+	}
+});
+function setChat(theId){
+	$("#selectedChat").val(theId);
+	alert($("#selectedChat").val());
+}
---
+++ lib/API/getConvoList.php
@@ -0,0 +1,35 @@
+<?php 
+require_once $_SERVER ['DOCUMENT_ROOT'] . "/lib/tool/php_bootstrap.php";
+$con = (new MySQL())->getConnection();
+$self = siteUser::convertToId($_COOKIE['USERNAME']);
+$has = Array();
+$data;
+$return;
+$list;
+$sql = 'SELECT * FROM `iCommission_Conversations` WHERE (`sender` = "'.$self.'" OR `toSendTo` = "'.$self.'") ORDER BY `ID` DESC';
+if (! $result = $con->query ( $sql )) {
+	die ( "There was an error running the query [" . $db->error . ']' );
+}
+while ( $row = $result->fetch_assoc () ) {
+	if($row['sender'] == $self){
+		if(!in_array($row['toSendTo'], $has)){
+			$data['icon'] = (new siteUser($row['toSendTo']))->getAvatar();
+			$data['user'] =  (new siteUser($row['toSendTo']))->getName();
+			$data['content'] = $row['text'];
+			$data['id'] = $row['toSendTo'];
+			$list[] = $data;
+			$has[]  = $row['toSendTo'];
+		}
+	}else{
+		if(!in_array($row['sender'], $has)){
+			$data['icon'] = (new siteUser($row['sender']))->getAvatar();
+			$data['user'] =  (new siteUser($row['sender']))->getName();
+			$data['id'] = $row['sender'];
+			$data['content'] = $row['text'];
+			$list[] = $data;
+			$has[] = $row['sender'];
+		}
+	}
+}
+$return['list'] = $list;
+die(json_encode($return));
